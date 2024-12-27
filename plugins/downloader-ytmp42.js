8dfd2d7e4e5954022ef38ffd1f00359af75a1c86
Fabián Cabañas <135176421+RHLMrodri7@users.noreply.github.com>
2024-12-27 17:22:33 -0300

    Create downloader-ytmp42.js

---
+++ plugins/downloader-ytmp42.js
@@ -0,0 +1,38 @@
+
+import fetch from 'node-fetch'
+
+let handler = async (m, { conn, text, usedPrefix, command }) => {
+let [url, resolution] = text.split(' ')
+if (!url) {
+return conn.reply(m.chat, `Ingresa el link de un video de youtube y una calidad ejemplo : ${usedPrefix + command} + *link* *360* `, m)
+}
+    
+try {
+let apiinfo = await fetch(`https://ytdownloader.nvlgroup.my.id/info?url=${url}`);
+let jsoninfo = await apiinfo.json()
+let titulo = jsoninfo.title
+let duracion = jsoninfo.duration || '-'
+let calidad = resolution || '360'
+let img = jsoninfo.thumbnail
+let dl_url = `https://ytdownloader.nvlgroup.my.id/download?url=${url}&resolution=${calidad}`
+let vidFetch = await fetch(dl_url)
+let video = await vidFetch.buffer()
+let Tamaño = video.length / (1024 * 1024)
+
+let HS = `- *Titulo* : ${titulo}
+- *Link* : ${url}
+- *Duracion* : ${duracion}
+- *Calidad* ${calidad}`
+if (Tamaño > 100) {
+await conn.sendMessage(m.chat, { document: video, caption: HS, mimetype: 'video/mp4', fileName: `${titulo}.mp4`})
+} else {
+await conn.sendMessage(m.chat, { video: video, caption: HS, mimetype: 'video/mp4'})
+}
+} catch (error) {
+console.error(error)    
+}}
+
+handler.command = ['ytmp4']
+handler.help = ['ytmp42']
+handler.tags = ['downloaer']
+export default handler
