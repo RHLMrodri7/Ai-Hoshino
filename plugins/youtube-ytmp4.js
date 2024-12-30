20619751328b179fd663f35e0f7220733b7032e0
Fabián Cabañas <135176421+RHLMrodri7@users.noreply.github.com>
2024-12-30 00:07:03 -0300

    Update youtube-ytmp4.js

--- plugins/youtube-ytmp4.js
+++ plugins/youtube-ytmp4.js
@@ -1,31 +1,22 @@
-import Starlights from '@StarlightsTeam/Scraper'
-import fetch from 'node-fetch' 
-let limit = 100
+import fetch from 'node-fetch'
 
-let handler = async (m, { conn, args, text, isPrems, isOwner, usedPrefix, command }) => {
-if (!args[0]) return conn.reply(m.chat, '[ ✰ ] Ingresa el enlace del vídeo de *YouTube* junto al comando.\n\n`» Ejemplo :`\n' + `> *${usedPrefix + command}* https://youtu.be/QSvaCSt8ixs`, m, rcanal)
-
-await m.react('🕓')
+let HS = async (m, { conn, text, usedPrefix, command }) => {
+if (!text) {
+return conn.reply(m.chat, `❀ Ingresa el link de un video de youtube `, m)
+}
+    
 try {
-let { title, size, quality, thumbnail, dl_url } = await Starlights.ytmp4(args[0])
+let calidad = '360' // Calidades disponibles : 144, 240, 360, 480, 720, 1080, 1440, 2160
+let api = await fetch(`https://api.giftedtech.my.id/api/download/dlmp4q?apikey=gifted&quality=${calidad}&url=${text}`)
+let json = await api.json()
+let { quality, title, download_url, thumbnail } = json.result
 
-let img = await (await fetch(`${thumbnail}`)).buffer()
-if (size.split('MB')[0] >= limit) return conn.reply(m.chat, `El archivo pesa mas de ${limit} MB, se canceló la Descarga.`, m, rcanal).then(_ => m.react('✖️'))
-	let txt = '`乂  Y O U T U B E  -  M P 4`\n\n'
-       txt += `	✩   *Titulo* : ${title}\n`
-       txt += `	✩   *Calidad* : ${quality}\n`
-       txt += `	✩   *Tamaño* : ${size}\n\n`
-       txt += `> *- ↻ El vídeo se esta enviando espera un momento, soy lenta. . .*`
-await conn.sendFile(m.chat, img, 'thumbnail.jpg', txt, m, null, rcanal)
-await conn.sendMessage(m.chat, { video: { url: dl_url }, caption: `${title}`, mimetype: 'video/mp4', fileName: `${title}` + `.mp4`}, {quoted: m })
-await m.react('✅')
-} catch {
-await m.react('✖️')
+
+await conn.sendMessage(m.chat, { video: { url: download_url }, caption: `${title}`, mimetype: 'video/mp4', fileName: `${title}` + `.mp4`}, {quoted: m })
+} catch (error) {
+console.error(error)
 }}
-handler.help = ['ytmp4 *<link yt>*']
-handler.tags = ['downloader']
-handler.command = ['ytmp4', 'ytv', 'yt']
-//handler.limit = 1
-handler.register = true 
 
-export default handler
+HS.command = /^(ytmp4)$/i
+
+export default HS
