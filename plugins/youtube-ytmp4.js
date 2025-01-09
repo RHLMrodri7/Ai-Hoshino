8e13a8de677ba7bcf127f0cde3284888d2d8586b
Fabián Cabañas <135176421+RHLMrodri7@users.noreply.github.com>
2025-01-09 20:35:45 -0300

    Update youtube-ytmp4.js

--- plugins/youtube-ytmp4.js
+++ plugins/youtube-ytmp4.js
@@ -1,3 +1,21 @@
+/* 
+
+*❀ By JTxs*
+
+[ Canal Principal ] :
+https://whatsapp.com/channel/0029VaeQcFXEFeXtNMHk0D0n
+
+[ Canal Rikka Takanashi Bot ] :
+https://whatsapp.com/channel/0029VaksDf4I1rcsIO6Rip2X
+
+[ Canal StarlightsTeam] :
+https://whatsapp.com/channel/0029VaBfsIwGk1FyaqFcK91S
+
+[ HasumiBot FreeCodes ] :
+https://whatsapp.com/channel/0029Vanjyqb2f3ERifCpGT0W
+*/
+
+// *[ ❀ YTMP4 ]*
 import fetch from 'node-fetch'
 
 let handler = async (m, { conn, command, text, usedPrefix }) => {
@@ -20,6 +38,5 @@ console.error(error)
 }}
 
 handler.command = /^(ytmp4)$/i
-handler.help = ['ytmp4']
-handler.tags = ['downloader']
+
 export default handler
