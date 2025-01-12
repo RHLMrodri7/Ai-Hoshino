4af7439269a2ff701180a19012f3b530c5bd3866
Fabián Cabañas <135176421+RHLMrodri7@users.noreply.github.com>
2025-01-12 14:42:21 -0300

    Create downloader-mf2.js

---
+++ plugins/downloader-mf2.js
@@ -0,0 +1,23 @@
+import fetch from 'node-fetch';
+
+let HS = async (m, { conn, text }) => {
+if (!text) return conn.reply(m.chat, `❀ Ingresa un link de mediafire`, m)
+
+try {
+let api = await fetch(`https://api.siputzx.my.id/api/d/mediafire?url=${text}`)
+let json = await api.json()
+let { filename, type, size, uploaded, ext, mimetype, download:dl_url } = json.data.response
+m.reply(`*${filename}*
+
+- *Tipo :* ${type}
+- *Tamaño :* ${size}
+- *Creado :* ${uploaded}`)
+await conn.sendFile(m.chat, dl_url, filename, null, m, null, { mimetype: ext, asDocument: true })
+
+} catch (error) {
+console.error(error)
+}}
+
+HS.command = ['mediafire']
+
+export default HS
