479333ba2a3047cd471b6d1d93fdc0dd54e43dc9
Fabián Cabañas <135176421+RHLMrodri7@users.noreply.github.com>
2025-01-12 14:51:21 -0300

    Update downloader-mf2.js

--- plugins/downloader-mf2.js
+++ plugins/downloader-mf2.js
@@ -6,13 +6,13 @@ if (!text) return conn.reply(m.chat, `❀ Ingresa un link de mediafire`, m)
 try {
 let api = await fetch(`https://api.siputzx.my.id/api/d/mediafire?url=${text}`)
 let json = await api.json()
-let { filename, type, filesize, uploaded, ext, mimetype, download:dl_url } = json.data.response
-m.reply(`*${filename}*
+let { FileName, Type, FileSize, Uploaded, ext, mimetype, download:dl_url } = json.data.response
+m.reply(`*${FileName}*
 
-- *Tipo :* ${type}
-- *Tamaño :* ${filesize}
-- *Creado :* ${uploaded}`)
-await conn.sendFile(m.chat, dl_url, filename, null, m, null, { mimetype: ext, asDocument: true })
+- *Tipo :* ${Type}
+- *Tamaño :* ${FileSize}
+- *Creado :* ${Uploaded}`)
+await conn.sendFile(m.chat, dl_url, FileName, null, m, null, { mimetype: ext, asDocument: true })
 
 } catch (error) {
 console.error(error)
