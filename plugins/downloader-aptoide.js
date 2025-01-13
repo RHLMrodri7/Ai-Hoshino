eb94753fb6c306136090ad36fdf6a00d508b438f
Fabián Cabañas <135176421+RHLMrodri7@users.noreply.github.com>
2025-01-13 20:20:31 -0300

    Update downloader-aptoide.js

--- plugins/downloader-aptoide.js
+++ plugins/downloader-aptoide.js
@@ -1,36 +1,23 @@
-/* 
-*❀ By JTxs*
-
-[ Canal Principal ] :
-https://whatsapp.com/channel/0029VaeQcFXEFeXtNMHk0D0n
-
-[ Canal Rikka Takanashi Bot ] :
-https://whatsapp.com/channel/0029VaksDf4I1rcsIO6Rip2X
-
-[ Canal StarlightsTeam] :
-https://whatsapp.com/channel/0029VaBfsIwGk1FyaqFcK91S
-
-[ HasumiBot FreeCodes ] :
-https://whatsapp.com/channel/0029Vanjyqb2f3ERifCpGT0W
-*/
-
-// *[ ❀ APK DL ]*
 import fetch from 'node-fetch'
 
 let handler = async (m, { conn, text }) => {
 if (!text) {
-return m.reply("❀ Ingresa el nomnbre de la app que quieres buscar")
+return m.reply("Ingresa el nombre de la app que quieres buscar")
 }
     
 try {
-let api = await fetch(`https://api.giftedtech.my.id/api/download/apkdl?apikey=gifted&appName=${text}`)
+let api = await fetch(`https://api.dorratz.com/v2/apk-dl?${text}`)
 let json = await api.json()
-let { appname, appicon, developer, download_url, mimetype } = json.result
-let txt = `- *Nombre* : ${appname}
-- *Creador* : ${developer}`
+let { name, creator, package, dllink, lastUpdate, size, icon,  mimetype } = json.result
+let txt = `- *Nombre* : ${name}
+- *Creador* : ${creator}
+- *Package*: ${package}
+- *Link*: ${dllink}
+- *Fecha*: ${lastUpdate}
+- *Peso*: ${size} `
 
-await conn.sendFile(m.chat, appicon, 'HasumiBotFreeCodes.jpg', txt, m)
-await conn.sendMessage(m.chat, { document: { url: download_url }, mimetype: mimetype, fileName: appname + '.apk', caption: null }, {quoted: m})
+await conn.sendFile(m.chat, icon, image.jpg', txt, m)
+await conn.sendMessage(m.chat, { document: { url: dllink }, mimetype: mimetype, name: name + '.apk', caption: null }, {quoted: m})
 } catch (error) {
 console.error(error)
 }}
