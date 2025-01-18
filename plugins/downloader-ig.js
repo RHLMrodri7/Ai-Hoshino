08215f1ae747cdacdabab8ef539825ea2a3c9281
Fabián Cabañas <135176421+RHLMrodri7@users.noreply.github.com>
2025-01-18 14:31:47 -0300

    Update downloader-ig.js

--- plugins/downloader-ig.js
+++ plugins/downloader-ig.js
@@ -1,18 +1,66 @@
-import Starlights from '@StarlightsTeam/Scraper'
+/* 
+
+*❀ By JTxs*
+
+[ Canal Principal ] :
+https://whatsapp.com/channel/0029VaeQcFXEFeXtNMHk0D0n
+
+[ Canal Rikka Takanashi Bot ] :
+https://whatsapp.com/channel/0029VaksDf4I1rcsIO6Rip2X
+
+[ Canal StarlightsTeam] :
+https://whatsapp.com/channel/0029VaBfsIwGk1FyaqFcK91S
+
+[ HasumiBot FreeCodes ] :
+https://whatsapp.com/channel/0029Vanjyqb2f3ERifCpGT0W
+*/
+
+// *[ ❀ INSTAGRAM DL (imagen/video) ]*
+import axios from 'axios'
 
 let handler = async (m, { conn, args, usedPrefix, command }) => {
-if (!args[0]) return conn.reply(m.chat, '🚩 Ingresa el enlace del vídeo de Instagram junto al comando.\n\n`Ejemplo:`\n' + `> *${usedPrefix + command}* https://www.instagram.com/p/C60xXk3J-sb/?igsh=YzljYTk1ODg3Zg==`, m, rcanal)
-await m.react('🕓')
+if (!args[0]) return conn.reply(m.chat,`❀ Ingresa un enlace de Instagram`, m)
+    
 try {
-let { dl_url } = await Starlights.igdl(args[0])
-await conn.sendFile(m.chat, dl_url, 'igdl.mp4', listo, m, null, rcanal)
-await m.react('✅')
-} catch {
-await m.react('✖️')
+let api = await axios.get(`https://restapi.apibotwa.biz.id/api/igdl?url=${args[0]}`)
+let json = api.data
+    
+let media = json.data
+let titulo = media.metadata.caption || ''
+let likes = media.metadata.like || ''
+let comentarios = media.metadata.comment || ''
+let mediaUrl = media.url
+
+    
+let HS = `- *Titulo :* ${titulo}
+
+- *Likes :* ${toNum(likes)} 
+- *Comentarios :* ${toNum(comentarios)} `
+      
+if (media.metadata.isVideo) {
+await conn.sendFile(m.chat, mediaUrl[0], 'instagram.mp4', HS, m)
+} else {
+for (let url of mediaUrl) {
+await conn.sendFile(m.chat, url, 'instagram.jpg', HS, m)
+}}    
+} catch (error) {
+console.error(error)    
 }}
-handler.help = ['instagram *<link ig>*']
+    
+handler.command = ['instagramdl', 'igdl']
 handler.tags = ['downloader']
-handler.command = /^(instagramdl|instagram|igdl|ig|instagramdl2|instagram2|igdl2|ig2|instagramdl3|instagram3|igdl3|ig3)$/i
-//handler.limit = 1
-handler.register = true 
+handler.help = ['ig <link>']
 export default handler
+
+function toNum(number) {
+if (number >= 1000 && number < 1000000) {
+return (number / 1000).toFixed(1) + 'k'
+} else if (number >= 1000000) {
+return (number / 1000000).toFixed(1) + 'M'
+} else if (number <= -1000 && number > -1000000) {
+return (number / 1000).toFixed(1) + 'k'
+} else if (number <= -1000000) {
+return (number / 1000000).toFixed(1) + 'M'
+} else {
+return number.toString()
+}}
