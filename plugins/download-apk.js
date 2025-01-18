a919d45384502892bcf0d36c60261af9a9447953
Fabián Cabañas <135176421+RHLMrodri7@users.noreply.github.com>
2025-01-18 13:58:00 -0300

    Update download-apk.js

--- plugins/download-apk.js
+++ plugins/download-apk.js
@@ -6,14 +6,14 @@ return m.reply("❀ Ingresa el nomnbre de la app que quieres buscar")
 }
     
 try {
-let api = await fetch('https://delirius-apiofc.vercel.app/download/apk?query=${text}`)
+let api = await fetch('https://delirius-apiofc.vercel.app/download/apk?=${text}`)
 let json = await api.json()
 let { name, image, developer, download, mimetype } = json.result
 let txt = `- *Nombre* : ${name}
 - *Creador* : ${developer}`
 
 await conn.sendFile(m.chat, image, 'HasumiBotFreeCodes.jpg', txt, m)
-await conn.sendMessage(m.chat, { document: { url: download }, mimetype: mimetype, fileName: appname + '.apk', caption: null }, {quoted: m})
+await conn.sendMessage(m.chat, { document: { url: download }, mimetype: mimetype, fileName: name + '.apk', caption: null }, {quoted: m})
 } catch (error) {
 console.error(error)
 }}
